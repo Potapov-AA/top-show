dfd5016a801e9e5dff815c68cbf3ba518e680a77
Alexandr Potapov <San_sanih99@mail.ru>
2024-06-11 21:48:20 +0300

    Добавил модель для теста

---
+++ backend/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+
+    }
+}
